e13a62524045a4361294cef48f191abdd980e052
corn <dalcon10280@gmail.com>
2024-08-23 18:21:49 +0900

    fix: modify session per method to prevent memory leaks

--- common/discord/discord.py
+++ common/discord/discord.py
@@ -1,12 +1,34 @@
 import json
+import asyncio
+import logging
 
-from typing import Dict, Any, Optional
-from dataclasses import dataclass
-from common.discord.discord_rest import DiscordEmbed, get_discord_rest
+from typing import Dict, Any, Optional, List
 from common.discord.message_color import MessageColor
 from common.discord.settings import get_topic
 from fastapi import HTTPException
+from aiohttp import ClientSession, TCPConnector, ClientTimeout
+from dataclasses import dataclass, asdict
 
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class DiscordEmbed:
+    """
+    https://discord.com/developers/docs/resources/webhook#execute-webhook-jsonform-params
+    """
+    author: Optional[Dict[str, Optional[str]]]
+    color: Optional[int] = None
+    fields: List[Dict[str, Optional[Any]]] = None
+    title: Optional[str] = None
+    # description: Optional[str] = None
+    # footer: Optional[Dict[str, Optional[str]]] = None
+    # image: Optional[Dict[str, Optional[Union[str, int]]]] = None
+    # provider: Optional[Dict[str, Any]] = None
+    # thumbnail: Optional[Dict[str, Optional[Union[str, int]]]] = None
+    # timestamp: Optional[str] = None
+    # url: Optional[str] = None
+    # video: Optional[Dict[str, Optional[Union[str, int]]]] = None
 
 
 @dataclass
@@ -15,6 +37,23 @@ class DiscordMessage:
     title: str
     message: str
     data: Optional[Dict[str, Any]]
+
+
+async def execute(url: str, embed: DiscordEmbed) -> None:
+    json = {
+        "embeds": [asdict(embed)],
+    }
+
+    async with ClientSession(
+            headers={"Content-Type": "application/json"},
+            connector=TCPConnector(ssl=False, limit=30),
+            timeout=ClientTimeout(3),
+            loop=asyncio.get_event_loop(),
+    ) as session:
+        async with session.post(url, json=json) as response:
+            if response.status != 204:
+                logger.error(f"Failed to execute webhook: {response.status}")
+                raise Exception(f"Failed to execute webhook: {response.status}")
 
 
 async def notify(message: DiscordMessage, color: MessageColor) -> None:
@@ -27,8 +66,7 @@ async def notify(message: DiscordMessage, color: MessageColor) -> None:
                                 "value": f"```json\n{json.dumps(message.data[x], indent=4, ensure_ascii=False)}\n```"},
                      message.data)]
     )
-    async with get_discord_rest() as rest:
-        await rest.execute(topic.url, embed)
+    await execute(topic.url, embed)
 
 
 async def notify_danger(message: DiscordMessage) -> None:
