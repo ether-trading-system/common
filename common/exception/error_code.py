b97c9d30305a11a5522b53121b7a0a425c8be1c0
corn <dalcon10280@gmail.com>
2024-09-09 02:22:00 +0900

    feat: add custom exception handlers

---
+++ common/exception/error_code.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class ErrorCode(Enum):
+    INVALID_REQUEST = 'INVALID_REQUEST'
+    UNKNOWN_ERROR = 'UNKNOWN_ERROR'
---
+++ common/exception/exception_handler.py
@@ -0,0 +1,57 @@
+from http import HTTPStatus
+from typing import Any, Optional
+from common import EtherException
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from dataclasses import dataclass
+from pydantic import ValidationError
+from pydantic_core import ErrorDetails
+from .error_code import ErrorCode
+
+
+@dataclass
+class EtherErrorResponse:
+    error_code: ErrorCode
+    message: str
+    detail: str
+    data: Optional[Any] = None
+
+
+def register_exception_handler(app: FastAPI) -> None:
+
+    @app.exception_handler(Exception)
+    async def ether_exception_handler(request: Request, exception: Exception):
+        return JSONResponse(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+            content=EtherErrorResponse(
+                error_code=ErrorCode.UNKNOWN_ERROR,
+                message=str(exception),
+                detail=str(exception),
+            ).__dict__
+        )
+
+    @app.exception_handler(ValidationError)
+    async def validation_error_handler(request: Request, exception: ValidationError):
+        errors: list[ErrorDetails] = exception.errors()
+
+        return JSONResponse(
+            status_code=HTTPStatus.BAD_REQUEST,
+            content=EtherErrorResponse(
+                error_code=ErrorCode.INVALID_REQUEST,
+                message=errors[0].get('msg'),
+                detail=errors[0].get('msg'),
+                data=exception.errors()
+            ).__dict__
+        )
+
+    @app.exception_handler(EtherException)
+    async def exception_handler(request: Request, exception: EtherException):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content=EtherErrorResponse(
+                error_code=exception.error_code,
+                message=exception.message,
+                detail=exception.detail,
+                data=exception.data
+            ).__dict__
+        )
