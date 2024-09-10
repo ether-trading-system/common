842c17bf0f2a797c34054e9f46d1cc1064118e88
kimdaewhi <kimdaewhi@naver.com>
2024-09-10 10:42:50 +0900

    [2024.09.10] dbManager를 위한 패키지 설치     - sqlalchemy ver 2.0.34     - psycopg2 ver 2.9.9
    postgresql_helper.py 파일 추가(신규)

---
+++ common/utils/postgresql_helper.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# PostgreSQL URL 설정
+DATABASE_URL = f"postgresql://postgres:1q2w3e4r!!@localhost:5432/nebula"
+
+# SQLAlchemy 엔진 생성
+engine = create_engine(DATABASE_URL)
+
+# 세션로컬 생성 : DB 요청 보내기 위한 파이프라인 역할
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+# Base 클래스 생성(모든 모델이 상속할 기본 클래스)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
